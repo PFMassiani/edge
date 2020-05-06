d765f4c573aede6e1a92e34f014dc764e390bbdd
Pierre-François Massiani <pfmassiani@gmail.com>
2020-05-06 12:51:14 +0200

    Add utilities for DiscreteStateActionSpace. Refactor.

--- edge/space/__init__.py
+++ edge/space/__init__.py
@@ -1,4 +1,6 @@
-from .space import DiscreteProductSpace, DiscreteStateActionSpace
+from .space import Space, DiscreteSpace, DiscreteProductSpace
+from .stateaction_space import DiscreteStateActionSpace
 from .box import Segment, Box
 
-__all__ = ['Segment', 'Box']
+__all__ = ['Segment', 'Box', 'DiscreteStateActionSpace',
+           'Space', 'DiscreteSpace', 'DiscreteProductSpace']
---
+++ edge/space/stateaction_space.py
@@ -0,0 +1,17 @@
+from .space import DiscreteProductSpace
+
+
+class DiscreteStateActionSpace(DiscreteProductSpace):
+    def __init__(self, state_space, action_space):
+        super(DiscreteStateActionSpace, self).__init__(
+            state_space,
+            action_space
+        )
+        self.state_space = state_space
+        self.action_space = action_space
+
+    def action(self, stateaction):
+        return self.get_projection_on_space(stateaction, self.action_space)
+
+    def state(self, stateaction):
+        return self.get_projection_on_space(stateaction, self.state_space)
