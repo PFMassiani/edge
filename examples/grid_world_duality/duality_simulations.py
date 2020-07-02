70ff4120d473d75e7c7eefeeaa7c978f332323e4
Pierre-François Massiani <pfmassiani@gmail.com>
2020-07-02 21:18:44 +0200

    Check value function behaviour on the discretized hovership for different penalties

--- examples/grid_world_duality/duality_simulations.py
+++ examples/grid_world_duality/duality_simulations.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 
-from edge import ModelLearningSimulation
-from edge.envs import DiscreteHovership
+from edge import Simulation, ModelLearningSimulation
 from edge.agent import DiscreteQLearner
-from edge.reward import AffineReward
 from edge.model.safety_models import SafetyTruth
+from edge.graphics.plotter import DualityPlotter, QValuePlotter
 from edge.model.value_models import QLearning
-from edge.graphics.plotter import QValuePlotter
+from edge.envs import DiscreteHovership
+from edge.reward import AffineReward, ConstantReward
 
 
 class LowGoalHovership(DiscreteHovership):
@@ -20,11 +20,22 @@ class LowGoalHovership(DiscreteHovership):
         self.reward = reward
 
 
-class ConstrainedSimulation(ModelLearningSimulation):
-    def __init__(self, name, max_samples, greed, step_size, discount_rate,
-                 every, glie_start):
-        output_directory = Path(__file__).parent.resolve()
-        super(ConstrainedSimulation, self).__init__(output_directory, name,
+class PenalizedHovership(LowGoalHovership):
+    def __init__(self, penalty_level=100, dynamics_parameters=None):
+        super(PenalizedHovership, self).__init__(dynamics_parameters)
+
+        def penalty_condition(state, action, new_state, reward):
+            return self.is_failure_state(new_state)
+
+        penalty = ConstantReward(self.reward.stateaction_space, -penalty_level,
+                                 reward_condition=penalty_condition)
+
+        self.reward += penalty
+
+
+class DualitySimulation(ModelLearningSimulation):
+    def __init__(self, output_directory, name, penalty, greed, step_size, discount_rate, max_samples, glie_start=None):
+        super(DualitySimulation, self).__init__(output_directory, name,
                                                     None)
         dynamics_parameters = {
             'ground_gravity': 1,
@@ -34,22 +45,29 @@ class ConstrainedSimulation(ModelLearningSimulation):
             'minimum_gravity_altitude': 9,
             'maximum_gravity_altitude': 3
         }
-        self.env = LowGoalHovership(dynamics_parameters=dynamics_parameters)
+        if penalty is None:
+            self.env = LowGoalHovership(dynamics_parameters=dynamics_parameters)
+        else:
+            self.env = PenalizedHovership(penalty_level=penalty,
+                                          dynamics_parameters=dynamics_parameters)
 
-        self.ground_truth = self.get_ground_truth()
-
+        if penalty is None:
+            self.ground_truth = self.get_ground_truth()
+            safety_threshold = 0
+        else:
+            self.ground_truth = None
+            safety_threshold = None
         self.agent = DiscreteQLearner(self.env, greed, step_size, discount_rate,
                                       constraint=self.ground_truth,
-                                      safety_threshold=0)
+                                      safety_threshold=safety_threshold)
 
         plotters = {
             'Q-Values': QValuePlotter(self.agent, self.agent.constraint)
         }
-
         self.plotters = plotters
 
+
         self.max_samples = max_samples
-        self.every = every
         self.glie_start = glie_start
 
     def get_models_to_save(self):
@@ -83,7 +101,6 @@ class ConstrainedSimulation(ModelLearningSimulation):
 
     def run(self):
         n_samples = 0
-        self.save_figs(prefix='0')
         while n_samples < self.max_samples:
             reset_state = self.agent.get_random_safe_state()
             self.agent.reset(reset_state)
@@ -106,31 +123,75 @@ class ConstrainedSimulation(ModelLearningSimulation):
                 if n_samples >= self.max_samples or old_state == new_state:
                     break
 
-        self.save_figs(prefix='final')
+        self.save_figs(prefix=f'{self.name}_final')
 
     def on_run_iteration(self, n_samples, old_state, action, new_state,
                                       reward, failed):
-        super(ConstrainedSimulation, self).on_run_iteration(old_state, action,
+        super(DualitySimulation, self).on_run_iteration(old_state, action,
                                                           new_state, reward,
                                                           failed)
 
         print(f'Iteration {n_samples}/{self.max_samples}: {self.agent.greed} | '
               f'{old_state} -> {action} -> {new_state} ({reward})')
-        if n_samples % self.every == 0:
-            self.save_figs(prefix=f'{n_samples}')
+
+
+class DualitySimulationsGroup(Simulation):
+    def __init__(self, name, penalties, greed, step_size, discount_rate, max_samples, glie_start=None):
+        output_directory = Path(__file__).parent.resolve()
+        super(DualitySimulationsGroup, self).__init__(output_directory, name, None)
+        self.constrained_sim = DualitySimulation(
+            output_directory=self.output_directory / 'simulations',
+            name='constrained',
+            penalty=None,
+            greed=greed,
+            step_size=step_size,
+            discount_rate=discount_rate,
+            max_samples=max_samples,
+            glie_start=glie_start
+        )
+        self.penalized_sims = [
+            DualitySimulation(
+                output_directory=self.output_directory / 'simulations',
+                name=f'penalized_{p}',
+                penalty=p,
+                greed=greed,
+                step_size=step_size,
+                discount_rate=discount_rate,
+                max_samples=max_samples,
+                glie_start=glie_start
+            ) for p in penalties
+        ]
+
+        self.plotters = {
+            'values': DualityPlotter(
+                penalties,
+                self.constrained_sim.agent,
+                *[pen_sim.agent for pen_sim in self.penalized_sims])
+        }
+
+    def run(self):
+        self.constrained_sim.run()
+        for pen_sim in self.penalized_sims:
+            pen_sim.run()
+        self.save_figs(prefix='small_penalties_final')
+
+    def save_models(self, globally=False, locally=True):
+        self.constrained_sim.save_models(globally, locally)
+        for pen_sim in self.penalized_sims:
+            pen_sim.save_models(globally, locally)
 
 
 if __name__ == '__main__':
-    sim = ConstrainedSimulation(
-        name='constrained',
-        max_samples=10000,
+    sim_group = DualitySimulationsGroup(
+        name='test',
+        penalties=[0, 4, 8, 12, 15],
         greed=0.1,
         step_size=0.6,
-        discount_rate=0.9,
-        every=1000,
+        discount_rate=0.3,
+        max_samples=10000,
         glie_start=7000
     )
-    sim.set_seed(0)
+    sim_group.set_seed(0)
 
-    sim.run()
-    sim.save_models()
+    sim_group.run()
+    sim_group.save_models()
