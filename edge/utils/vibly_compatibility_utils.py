6dd6d997f06fd4ec8645db8007f472cb82d6188f
Steve Heim <heim.steve@gmail.com>
2020-07-03 11:06:44 +0200

    add compatibility for SLIP

--- edge/utils/vibly_compatibility_utils.py
+++ edge/utils/vibly_compatibility_utils.py
@@ -1,3 +1,5 @@
+import warnings
+
 HOVERSHIP_LOOKUP_DICTIONARY = {
     'base_gravity': 'ground_gravity',
     'gravity': 'gravity_gradient',
@@ -8,10 +10,26 @@ HOVERSHIP_LOOKUP_DICTIONARY = {
     'thrust': None
 }
 
+SLIP_LOOKUP_DICTIONARY = {
+    'gravity': 'gravity',
+    'mass': 'mass',
+    'stiffness': 'stiffness',
+    'resting_length': 'resting_length',
+    'total_energy': 'energy',
+    'angle_of_attack': None,
+    'actuator_resting_length': None,
+    'x0': None
+}
+
 
 def get_parameters_lookup_dictionary(env):
-    from edge.envs import Hovership
+    from edge.envs import Hovership, Slip
     if isinstance(env, Hovership):
         return HOVERSHIP_LOOKUP_DICTIONARY
+    elif isinstance(env, Slip):
+        return SLIP_LOOKUP_DICTIONARY
     else:
+        warnings.warn("Seems like you haven't implemented some " +
+                      "compabibility stuff for your environment yet! See " +
+                      "`utils/vibly_compatibility_utils.py`")
         return None
