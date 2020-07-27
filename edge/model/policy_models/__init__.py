bba4907b6e58cefb14bb3403836ae7ceb01b04eb
Pierre-François Massiani <pfmassiani@gmail.com>
2020-07-27 11:41:17 +0200

    Fix MultilayerPerceptron import

--- edge/model/policy_models/__init__.py
+++ edge/model/policy_models/__init__.py
@@ -2,6 +2,6 @@ from .policy import Policy, RandomPolicy
 from .greedy import ConstrainedEpsilonGreedy, EpsilonGreedy
 from .safety import SafetyMaximization, SafetyActiveSampling
 try:
-    from .multilayer_perceptron import MLPPolicy
+    from .multilayer_perceptron import MLPPolicy, MultilayerPerceptron
 except ImportError:
     print("Some functionality unavailable (probably AI Gym)")
