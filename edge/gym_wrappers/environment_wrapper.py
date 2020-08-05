3158f85dd844848145125ae201d7f32c1ab84145
Pierre-François Massiani <pfmassiani@gmail.com>
2020-08-05 17:49:19 +0200

    Specify shape when wrapping a Gym env; Add support for Gym environments in addition to Safety-Gym

--- edge/gym_wrappers/environment_wrapper.py
+++ edge/gym_wrappers/environment_wrapper.py
@@ -13,11 +13,23 @@ class DummyDynamics:
         return True
 
 
+def get_index_length(gym_box):
+    return len(gym_box.low.reshape(-1))
+
+
 class GymEnvironmentWrapper(Environment):
-    def __init__(self, gym_env, failure_critical = False):
+    def __init__(self, gym_env, shape=None, failure_critical = False):
         self.gym_env = gym_env
+        if shape is None:
+            obs_shape = None
+            action_shape = None
         if isinstance(gym_env.action_space, gspaces.Box):
-             action_space = BoxWrapper(gym_env.action_space)
+            if shape is not None:
+                action_space_ndim = get_index_length(gym_env.action_space)
+                action_shape = shape[-action_space_ndim:]
+            action_space = BoxWrapper(
+                gym_env.action_space, discretization_shape=action_shape
+            )
         elif isinstance(gym_env.action_space, gspaces.Discrete):
             action_space = DiscreteWrapper(gym_env.action_space)
         else:
@@ -25,7 +37,13 @@ class GymEnvironmentWrapper(Environment):
                              'and Discrete are currently supported')
 
         if isinstance(gym_env.observation_space, gspaces.Box):
-            state_space = BoxWrapper(gym_env.observation_space)
+            if shape is not None:
+                state_space_ndim = get_index_length(gym_env.observation_space)
+                obs_shape = shape[:state_space_ndim]
+            state_space = BoxWrapper(
+                gym_env.observation_space,
+                discretization_shape=obs_shape
+            )
         elif isinstance(gym_env.observation_space, gspaces.Discrete):
             state_space = DiscreteWrapper(gym_env.observation_space)
         else:
@@ -60,18 +78,30 @@ class GymEnvironmentWrapper(Environment):
         # raise a meaningful error for the user
         if s is not None:
             raise ValueError('Selecting the initial state is not supported for Gym environments')
-        self.gym_env.reset()
-        self.s = self.gym_env.obs()
+        reset_output = self.gym_env.reset()
+        # Safety gym does not return anything with reset, whereas Gym returns
+        # the state
+        if reset_output is None:
+            self.s = self.gym_env.obs()
+        else:
+            self.s = reset_output
         return self.s
 
     def step(self, action):
         gym_action = self.action_space.to_gym(action)
         if not self.failure_critical or not self.has_failed:
             gym_new_state, reward, done, info = self.gym_env.step(gym_action)
-            self.s = self.state_space.from_gym(gym_new_state)
+            s = self.state_space.from_gym(gym_new_state)
+            # Gym does not put a hard constraint on the fact that the state
+            # stays in the limit of the Box. Edge crashes if this happens, so
+            # we project the resulting state in state-space
+            self.s = self.state_space.closest_in(s)
             self.done = done
             self.info = info
         else:
             reward = 0
 
         return self.s, reward, self.has_failed
+
+    def render(self):
+        self.gym_env.render()
