e7b2281338ca275a284e83dd22081ee71884ed75
Pierre-François Massiani <pfmassiani@gmail.com>
2020-08-18 16:10:54 +0200

    Rename examples to experiments

--- experiments/run_experiment.sh
+++ experiments/run_experiment.sh
@@ -7,6 +7,6 @@ shift
 experiment_name=$1
 shift
 python_script_name=$1
-python_script_path="./examples/$experiment_name/$python_script_name"
+python_script_path="./experiments/$experiment_name/$python_script_name"
 shift
 python $python_script_path --rid $random_id $*
