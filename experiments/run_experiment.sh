a7c4f0f13e7c0578130b21e4c88051727818672a
Pierre-François Massiani <pfmassiani@gmail.com>
2020-08-21 20:15:03 +0200

    Edit run_experiment.sh to run on cluster

--- experiments/run_experiment.sh
+++ experiments/run_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-source /home/pifou/.virtualenvs/edge/bin/activate
+source venv/bin/activate
 process=$1
 shift
 experiment_name=$1
@@ -9,4 +9,4 @@ shift
 python_script_name=$1
 python_script_path="./experiments/$experiment_name/$python_script_name"
 shift
-python $python_script_path --rid $random_id $*
+python $python_script_path $random_id $*
